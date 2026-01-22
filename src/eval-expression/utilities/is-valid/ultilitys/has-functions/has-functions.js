f2475e80dd0f7e399119dc1f5b7c78313ccdb887
Zadoque <zadoquepiresdedeus@gmail.com>
2026-01-22 15:49:54 -0300

    has functions searches for letters before (

--- src/eval-expression/utilities/is-valid/ultilitys/has-functions/has-functions.js
+++ src/eval-expression/utilities/is-valid/ultilitys/has-functions/has-functions.js
@@ -1,6 +1,17 @@
+/**
+ * Checks if expression contains mathematical functions
+ * 
+ * @param {string} expression - The expression to check
+ * @returns {boolean} True if contains functions, false otherwise
+ * 
+ * @example
+ * hasFunctions("2+3")        // false
+ * hasFunctions("sqrt(9)")    // true
+ * hasFunctions("sin(30)+2")  // true
+ */
 const hasFunctions = (expression) => {
-    return false;
+    // Detecta padrão: letras minúsculas seguidas de '('
+    return /[a-z]+\(/.test(expression);
 };
-
 
 module.exports = hasFunctions;
